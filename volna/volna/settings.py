d2424894859f5d8f61a948cf8e6abc8979e3b8e2
Vyacheslav <vyacheslav.klyuev@gmail.com>
2025-06-18 16:39:17 +1000

    Изменения setting и базы данных

--- volna/volna/settings.py
+++ volna/volna/settings.py
@@ -90,8 +90,16 @@ WSGI_APPLICATION = 'volna.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Путь к вашей SQLite-базе
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'llleam5r_volna',      # Имя БД из панели Beget
+        'USER': 'llleam5r_volna',      # Пользователь БД
+        'PASSWORD': '%iVKTFfP3wlC',  # Пароль БД
+        'HOST': 'llleam5r.beget.tech',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
